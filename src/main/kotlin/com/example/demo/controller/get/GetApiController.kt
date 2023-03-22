56930e8d1afcd6a3406866af50936fa0ba158695
박준용 <jo101044@naver.com>
2023-03-22 15:30:57 +0900

    feat : different kinds of Get

--- src/main/kotlin/com/example/demo/controller/get/GetApiController.kt
+++ src/main/kotlin/com/example/demo/controller/get/GetApiController.kt
@@ -1,4 +1,62 @@
 package com.example.demo.controller.get
 
+import com.example.demo.model.http.UserRequest
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
 class GetApiController {
+
+    // 이전 방식
+    @GetMapping(path = ["/hello", "/abcd"])
+    fun hello(): String {
+        return "hello kotlin"
+    }
+
+    // 최근 방식
+    @RequestMapping(method = [RequestMethod.GET], path = ["/request-mapping"])
+    fun requestMapping(): String {
+        return "request-mapping"
+    }
+
+    @GetMapping("/get-mapping/path-variable/{name}/{age}")
+    fun pathVariable(@PathVariable name: String, @PathVariable age: Int): String {
+        println("$name $age")
+        return "$name $age"
+    }
+
+    @GetMapping("/get-mapping/path-variable2/{name}/{age}")
+    fun pathVariable2(@PathVariable(value = "name") _name: String, @PathVariable(name = "age") age: Int): String {
+        val name = "kotlin"
+
+        println("$_name $age")
+        return "$_name $age"
+    }
+
+    @GetMapping("/get-mapping/query-param")
+    fun queryParam(
+        @RequestParam name: String,
+        @RequestParam(value = "age") age: Int
+    ): String {
+        println("$name , $age")
+        return "$name $age"
+    }
+
+    @GetMapping("/get-mapping/query-param/object")
+    fun queryParamObject(userRequest: UserRequest): UserRequest {
+        println(userRequest)
+        return userRequest
+
+    }
+
+    @GetMapping("/get-mapping/query-param/map")
+    fun queryParamMap(@RequestParam map: Map<String, Any>): Map<String, Any>{
+        println(map)
+        return map
+    }
 }
