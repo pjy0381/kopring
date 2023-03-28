361640d0226b4c85c2746665d93c9e1e8856481c
박준용 <jo101044@naver.com>
2023-03-28 10:51:18 +0900

    feat : different kinds of Post

---
+++ src/main/kotlin/com/example/demo/controller/post/PostApiController.kt
@@ -0,0 +1,29 @@
+package com.example.demo.controller.post
+
+import com.example.demo.model.http.UserRequest
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
+class PostApiController {
+
+    @PostMapping("/post-mapping")
+    fun postMapping(): String{
+        return "post-mapping"
+    }
+
+    @RequestMapping(method = [RequestMethod.POST], path = ["/request-mapping"])
+    fun requestMapping(): String{
+        return "request-mapping"
+    }
+
+    @PostMapping("/post-mapping/object")
+    fun postMappingObject(@RequestBody userRequest: UserRequest): UserRequest{
+        println(userRequest)
+        return userRequest
+    }
+}
